626a9827c2db5c980444dfb424edc4764e49b4e4
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2025-01-02 03:17:57 +0100

    Easteregg added

--- todo-app/src/components/Header.jsx
+++ todo-app/src/components/Header.jsx
@@ -1,16 +1,39 @@
-export function Header(props) {
+import React, { useState } from 'react';
+import './Header.css';
 
-    const {todos} = props;
+export function Header(props) {
+    const { todos } = props;
     const todosLength = todos.length;
 
-    const isTasksPlural = todosLength != 1;
-    const taskOrTasks = isTasksPlural ? "tasks" : "task"
+    const isTasksPlural = todosLength !== 1;
+    const taskOrTasks = isTasksPlural ? "tasks" : "task";
+
+    const [showMessage, setShowMessage] = useState(false);
+
+    const handleClick = () => {
+        setShowMessage(true);
+    };
+
+    const handleClose = () => {
+        setShowMessage(false);
+    };
+
+    if (showMessage) {
+        return (
+            <div className="fullscreen-message">
+                <div className='message'>
+                <span className="heart">♥</span><span className="heart">♥</span><span className="heart">♥</span>I love you Lolita!<span className="heart">♥</span><span className="heart">♥</span><span className="heart">♥</span>
+                </div>
+                <button className="close-button" onClick={handleClose}>Close</button>
+            </div>
+        );
+    }
 
     return (
-        <header>
+        <header onClick={handleClick}>
             <h1 className="text-gradient">
                 You have {todosLength} open {taskOrTasks}.
             </h1>
         </header>
-    )
+    );
 }
